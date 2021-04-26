84a41d6e514e8ad8c7c067edc18aed0dfe631244
Paul Micheli <paul@micheli.co.uk>
2021-04-26 12:52:30 +0100

    refactor on SNG

--- sng/pseudonym.py
+++ sng/pseudonym.py
@@ -1,23 +1,32 @@
-import sys, random
+"""Generates funny names y randomly combining names from 2 separate lists."""
+import sys
+import random
 
-print("Welcome to the Psych 'Sidekick Name Picker.'\n")
-print("A name just like Sean would pick for Gus:\n\n")
 
-first = ('Baby Oil', 'Bad News')
+def main():
+    """Choose names at random from 2 tuples of names and prints to screen."""
+    print("Welcome to the Psych 'Sidekick Name Picker.'\n")
+    print("A name just like Sean would pick for Gus:\n\n")
 
-last = ('Appleyard', 'Bigmeat')
+    first = ('Baby Oil', 'Bad News')
 
-while True:
-    firstName = random.choice(first)
+    last = ('Appleyard', 'Bigmeat')
 
-    lastName = random.choice(last)
+    while True:
+        first_name = random.choice(first)
 
-    print("\n\n")
-    print("{} {}".format(firstName, lastName), file=sys.stderr)
-    print("\n\n")
+        last_name = random.choice(last)
 
-    try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
-    if try_again.lower() == "n":
-        break
+        print("\n\n")
+        print("{} {}".format(first_name, last_name), file=sys.stderr)
+        print("\n\n")
 
-input("\nPress Enter to exit.")
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
+        if try_again.lower() == "n":
+            break
+
+    input("\nPress Enter to exit.")
+
+
+if __name__ == "__main__":
+    main()
